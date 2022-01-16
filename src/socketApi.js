10bceadf296ed042f2c70b808a63c964814d08b4
nesimiyildiz <nesimi_yildiz@aof.anadolu.edu.tr>
2022-01-16 21:54:18 +0300

    ng-cloak

--- src/socketApi.js
+++ src/socketApi.js
@@ -3,8 +3,10 @@ const io=socketio();
 const socketApi={};
 socketApi.io=io;
 
-const users=[];
+const users={};
+//helpers
 
+const randomColor=require("../helpers/randomColor")
 io.on('connection',(socket)=>{
     console.log("a user connected")
     socket.on('newUser',(data)=>{
@@ -13,13 +15,39 @@ io.on('connection',(socket)=>{
             position:{
                 x:0,
                 y:0
-            }
+            },
+            color:randomColor()
         }
-        const userData=Object.assign(data,defaultData)
-        users.push(userData);
+        const userData = Object.assign(data, defaultData);
+        users[socket.id] = userData;
+
+        socket.broadcast.emit('newUser', users[socket.id]);
+        socket.emit('initPlayers', users);
+
+    });
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('disUser',users[socket.id]);
+        delete users[socket.id];
+    });
+    socket.on('animate',(data)=>{
+        try {
+            users[socket.id].position.x=data.x;
+            users[socket.id].position.y=data.y;
+        }catch (e){
+            console.log(e)
+        }
+
+
+        socket.broadcast.emit('animate',{socketId:socket.id,x:data.x,y:data.y})
 
     })
+    socket.on('newMessage',(data)=>{
+        const messageData=Object.assign({socketId:socket.id},data);
+        socket.broadcast.emit('newMessage',messageData);
+    })
+
 });
+
 
 
 module.exports=socketApi;
